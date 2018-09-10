b30b9c799e5959ae54c0a95891908627f97c3ea8
eakasakp <eakasakp@scg.com>
2018-09-10 23:20:50 +0700

    commit sentry test sending report errors.

---
+++ Sentry.Demo/Sentry.Demo.App/Sentry.cs
@@ -0,0 +1,23 @@
+﻿using SharpRaven;
+using SharpRaven.Data;
+using System;
+
+namespace Sentry.Demo.App
+{
+    class Sentry
+    {
+        static void Main(string[] args)
+        {
+            var ravenClient = new RavenClient("https://f1d4810614784612b8a94fefb2c86ca2@sentry.io/1277995");
+            try
+            {
+                int i2 = 0;
+                int i = 10 / i2;
+            }
+            catch (Exception exception)
+            {
+                ravenClient.Capture(new SentryEvent(exception));
+            }           
+        }
+    }
+}
